6e8f6f427d6e2e827201262bd13e47cac6a9e3ca
drr481 <drr481@alumnos.unican.es>
2024-03-07 12:57:40 +0100

    GestionEmpleados

--- Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionTiendas.java
+++ Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionTiendas.java
@@ -1,25 +1,62 @@
 package es.unican.is2.business;
 
+import java.util.HashMap;
 import es.unican.is2.ccommon.*;
 
 public class GestionTiendas implements IGestionTiendas {
 
+	public HashMap<String, Tienda> tiendas = new HashMap<>();
+	
 	@Override
 	public Tienda nuevaTienda(Tienda t) throws DataAccessException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		if (t != null) {
+			tiendas.put(t.getNombre(), t);
+		} else {
+			throw new DataAccessException();
+		}
+		
+		return t;
 	}
 
 	@Override
 	public Tienda eliminarTienda(String nombre) throws OperacionNoValidaException, DataAccessException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		if (tiendas.containsKey(nombre)) {
+			throw new OperacionNoValidaException("Tienda ya existe");
+		}
+		
+		Tienda n = null;
+		
+		if (nombre != null) {
+			
+			n = tiendas.get(nombre);
+			
+			if (n == null) {
+				throw new DataAccessException();
+			}
+		} else {
+			throw new DataAccessException();
+		}
+		
+		return n;
 	}
 
 	@Override
 	public Tienda tienda(String nombre) throws DataAccessException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		Tienda n = null;
+		
+		if (nombre != null) {
+			n = tiendas.get(nombre);
+		} else {
+			throw new DataAccessException();
+		}
+		
+		return n;
 	}
 
 }
