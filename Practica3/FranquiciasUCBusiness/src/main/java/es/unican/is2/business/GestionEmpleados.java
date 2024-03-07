937fc77465e718d7cb350f0c18eeace2b11f05fd
drr481 <drr481@alumnos.unican.es>
2024-03-07 12:23:59 +0100

    Correccion a Implementacion

--- Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionEmpleados.java
+++ Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionEmpleados.java
@@ -2,8 +2,32 @@ package es.unican.is2.business;
 
 import es.unican.is2.ccommon.*;
 
-public class GestionEmpleados {
+public class GestionEmpleados implements IGestionEmpleados {
+
+	@Override
+	public Empleado nuevoEmpleado(Empleado e, String nombre) throws OperacionNoValidaException, DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Empleado eliminarEmpleado(String dni, String nombre) throws OperacionNoValidaException, DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean trasladarEmpleado(String dni, String actual, String destino)
+			throws OperacionNoValidaException, DataAccessException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Empleado empleado(String dni) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
-	Empleado e = null;
 
 }
--- Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionTiendas.java
+++ Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionTiendas.java
@@ -2,6 +2,24 @@ package es.unican.is2.business;
 
 import es.unican.is2.ccommon.*;
 
-public class GestionTiendas {
+public class GestionTiendas implements IGestionTiendas {
+
+	@Override
+	public Tienda nuevaTienda(Tienda t) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tienda eliminarTienda(String nombre) throws OperacionNoValidaException, DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tienda tienda(String nombre) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
