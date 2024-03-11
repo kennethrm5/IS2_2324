94db8bf95e04cf3180a88bea50bef47ee27e82c5
drr481 <drr481@alumnos.unican.es>
2024-03-11 18:24:58 +0100

    Jar creado sin comprobar

--- Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionEmpleados.java
+++ Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/business/GestionEmpleados.java
@@ -2,13 +2,21 @@ package es.unican.is2.business;
 
 import java.util.HashMap;
 import es.unican.is2.ccommon.*;
+import es.unican.is2.dao.EmpleadosDAO;
 import es.unican.is2.dao.TiendasDAO;
 
 public class GestionEmpleados implements IGestionEmpleados {
 
-	private TiendasDAO tiendas = new TiendasDAO();
-	private HashMap<String, Tienda> empleados = new HashMap<>();
+	private TiendasDAO tiendas = null;
+	private EmpleadosDAO empleados = null;
+	private HashMap<String, Tienda> tiendaEmpleado = new HashMap<>();
 	
+	public GestionEmpleados(TiendasDAO tiendasDAO, EmpleadosDAO empleadosDAO) {
+		
+		tiendas = tiendasDAO;
+		empleados = empleadosDAO;
+	}
+
 	@Override
 	public Empleado nuevoEmpleado(Empleado e, String nombre) throws OperacionNoValidaException, DataAccessException {
 		try {
@@ -20,7 +28,8 @@ public class GestionEmpleados implements IGestionEmpleados {
 				throw new OperacionNoValidaException("El empleado ya existe.");
 			}
 			t.getEmpleados().add(e);
-			empleados.put(e.getDNI(), t);
+			empleados.crearEmpleado(e);
+			tiendaEmpleado.put(e.getDNI(), t);
 			return e;
 		} catch (DataAccessException e1) {
 			throw new DataAccessException();
@@ -29,51 +38,54 @@ public class GestionEmpleados implements IGestionEmpleados {
 
 	@Override
 	public Empleado eliminarEmpleado(String dni, String nombre) throws OperacionNoValidaException, DataAccessException {
-		try {
-			Tienda t = tiendas.tiendaPorNombre(nombre);
-			if (t == null) {
-				return null;
-			}
-			Empleado e = t.buscaEmpleado(dni);
-			if (e == null) {
-				throw new OperacionNoValidaException("El empleado no existe en esta tienda.");
-			}
-			t.getEmpleados().remove(e);
-			empleados.remove(e.getDNI());
-			return e;
-		} catch (DataAccessException e1) {
+
+		Tienda t = tiendas.tiendaPorNombre(nombre);
+		if (t == null) {
 			throw new DataAccessException();
 		}
+		Empleado e = t.buscaEmpleado(dni);
+		if (e == null) {
+			throw new OperacionNoValidaException("El empleado no existe en esta tienda.");
+		}
+		t.getEmpleados().remove(e);
+		empleados.eliminarEmpleado(e.getDNI());
+		tiendaEmpleado.remove(e.getDNI());
+		return e;
+
 	}
 
 	@Override
 	public boolean trasladarEmpleado(String dni, String actual, String destino)
 			throws OperacionNoValidaException, DataAccessException {
-		try {
-			Tienda t1 = tiendas.tiendaPorNombre(actual);
-			Tienda t2 = tiendas.tiendaPorNombre(destino);
-			if (!empleados.containsKey(dni) || t1 == null || t2  == null) {
-				return false;
-			}
-			Empleado e = t1.buscaEmpleado(dni);
-			if (!empleados.containsKey(dni)) {
-				throw new OperacionNoValidaException("El empleado ya existe.");
-			}
-			t1.getEmpleados().remove(e);
-			t1.getEmpleados().add(e);
-			empleados.replace(e.getDNI(), t2);
-			return true;
-		} catch (DataAccessException e1) {
+
+		Tienda t1 = tiendas.tiendaPorNombre(actual);
+		Tienda t2 = tiendas.tiendaPorNombre(destino);
+
+		if ( t1 == null || t2  == null) {
+			throw new OperacionNoValidaException("Las tiendas no existen.");
+		}
+		Empleado e = t1.buscaEmpleado(dni);
+		if ( !tiendaEmpleado.containsKey(dni)) {
 			throw new DataAccessException();
 		}
+
+		if (e == null) {
+			throw new DataAccessException();
+		}
+
+		t1.getEmpleados().remove(e);
+		t1.getEmpleados().add(e);
+		tiendaEmpleado.replace(e.getDNI(), t2);
+		return true;
+		
 	}
 
 	@Override
 	public Empleado empleado(String dni) throws DataAccessException {
-		if (!empleados.containsKey(dni)) {
+		if (!tiendaEmpleado.containsKey(dni)) {
 			return null;
 		}
-		Empleado e = empleados.get(dni).buscaEmpleado(dni);
+		Empleado e = tiendaEmpleado.get(dni).buscaEmpleado(dni);
 		if (e == null) {
 			throw new DataAccessException();
 		}
