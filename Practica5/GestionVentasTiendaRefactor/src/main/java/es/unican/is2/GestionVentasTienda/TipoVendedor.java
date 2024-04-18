27b57e544ef8c3e34ecb59fe10161898d6a05e1d
drr481 <drr481@alumnos.unican.es>
2024-04-18 12:21:47 +0200

    Projecto Refactor

---
+++ Practica5/GestionVentasTiendaRefactor/src/main/java/es/unican/is2/GestionVentasTienda/TipoVendedor.java
@@ -0,0 +1,10 @@
+package es.unican.is2.GestionVentasTienda;
+
+/**
+ * Enumerado que define los dos tipos de vendedores en plantilla
+ * que existen en la tienda
+ *
+ */
+public enum TipoVendedor { // WMC +1
+	Junior, Senior
+}
